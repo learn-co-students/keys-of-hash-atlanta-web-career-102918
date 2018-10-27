d06ee1a334c4081ecf2fbb3761d9ec586d06a2bd
github username <github email address>
2018-10-27 13:47:14 -0400

    My first commit

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,18 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    array_of_keys = []
+    self.each do |k, v|
+      # binding.pry
+      arguments.each do |argument|
+        if v == argument
+          # binding.pry
+          array_of_keys << k 
+        end
+      end
+    end
+    array_of_keys
   end
 end
